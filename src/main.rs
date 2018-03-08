1fb26c03edecff24e81f5795ae243157a07c78b4
André Ligné <hello@bzf.se>
2018-03-08 20:25:59 +0100

    Create Rocket application that serves static content
    It's built into the binary!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+use rocket::response::content::Html;
+
+#[get("/", format = "text/html")]
+fn html() -> Html<&'static str> {
+    let foo = include_str!("web/index.html");
+    Html(foo)
+}
+
+#[get("/index.js", format = "application/javascript")]
+fn javascript() -> &'static str {
+    include_str!("web/index.js")
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![html, javascript]).launch();
 }
